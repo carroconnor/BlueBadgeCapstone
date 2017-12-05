af2027ff93517a643e9d44046c73ecef84e7a6c3
Carr OConnor <carr.oconnor@gmail.com>
2017-12-05 13:39:34 -0500

    setup db connection

--- Messenger.Web/App_Start/Startup.Auth.cs
+++ Messenger.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Messenger.Web.Models;
+using Messenger.Data;
 
 namespace Messenger.Web
 {
--- Messenger.Web/Controllers/AccountController.cs
+++ Messenger.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Messenger.Web.Models;
+using Messenger.Data;
 
 namespace Messenger.Web.Controllers
 {
