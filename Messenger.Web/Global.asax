6259bdbffe13f4f134e2dbe09ea4900dc8a398ca
Carr OConnor <carr.oconnor@gmail.com>
2017-12-05 11:17:16 -0500

    Add project files.

---
+++ Messenger.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Messenger.Web.MvcApplication" Language="C#" %>
---
+++ Messenger.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Messenger.Web.Startup))]
+namespace Messenger.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
