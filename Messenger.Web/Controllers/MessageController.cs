790b94950a9dd4ba632a36d458380237b9adbe3f
Carr OConnor <carr.oconnor@gmail.com>
2017-12-06 15:32:17 -0500

    Message Create View

---
+++ Messenger.Web/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Messenger.Models;
+
+namespace Messenger.Web.Controllers
+{
+    [Authorize]
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            var model = new MessageListItem[0];
+            return View(model);
+        }
+
+        //Create Method
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MessageCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                 
+            }
+            return View(model);
+        }
+    }
+}
