3b11491c682f09d1df95755698367dda89cbffdc
Carr OConnor <carr.oconnor@gmail.com>
2017-12-22 15:00:31 -0500

    added MyMessages Model and View

---
+++ Messenger.Models/MyMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Models
+{
+    public class MyMessages
+    {
+        public int MessageId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
--- Messenger.Services/MessageService.cs
+++ Messenger.Services/MessageService.cs
@@ -35,6 +35,28 @@ namespace Messenger.Services
             }
         }
 
+        public IEnumerable<MyMessages> GetMyMessages()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Messages
+                        .Where(i => i.OwnerId = _userId)
+                        .Select(
+                            e =>
+                                new MessageListItem
+                                {
+                                    MessageId = e.MessageId,
+                                    Title = e.Title,
+                                    Content = e.Content,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+
         public IEnumerable<MessageListItem> GetMessages()
         {
             using (var ctx = new ApplicationDbContext())
